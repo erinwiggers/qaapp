d43bbdccee6795e44949c4da1191f05d0afa61f8
Erin <erin.wiggers@lyntonweb.com>
2018-05-22 17:55:57 -0500

    Results Array

--- qaapp/qaapp-script.js
+++ qaapp/qaapp-script.js
@@ -93,7 +93,7 @@ var parseResults = function (testData) {
             show_result = results[i].show_result_web_url,
             launch_live = results[i].launch_live_test_url
 
-        resultsArray += [ result_id, result_os, result_browser, result_resolution ];
+        resultsArray += [{ result_id, result_os, result_browser, result_resolution }];
     }
     console.log(resultsArray);
 };
