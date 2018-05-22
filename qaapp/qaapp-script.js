4e5759135fd1ec896061ce06475103990d0a1f9a
Erin <erin.wiggers@lyntonweb.com>
2018-05-22 18:15:44 -0500

    Test

--- qaapp/qaapp-script.js
+++ qaapp/qaapp-script.js
@@ -95,7 +95,7 @@ var parseResults = function (testData) {
 
         resultsArray += { "id": result_id, "os": result_os, "broswer": result_browser, "resolution": result_resolution };
     }
-    console.log(JSON.parse(resultsArray));
+    console.log(JSON.stringify(resultsArray));
 };
 
 // CREATE SPREADSHEET AND POPULATE WITH TEST DATA
